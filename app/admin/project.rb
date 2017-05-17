bd6ce1a4ad67ee10a136f2e5f10ee52fa388d955
Murugan <murugu2012@gmail.com>
2017-05-17 06:35:21 +0530

    Whitelist columns in Project for ActiveAdmin

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Project do
 
+ permit_params :title, :project_type, :description, :status, :version
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
